28ce54484c9940574cd82a6d143a83318f7d7754
semicontinuity <igor.a.karpov@gmail.com>
2025-06-07 12:22:43 +0300

    cleanup

--- src/main/java/semicontinuity/idea/code/analyzer/graph/DAGraphDecomposer.java
+++ src/main/java/semicontinuity/idea/code/analyzer/graph/DAGraphDecomposer.java
@@ -8,6 +8,8 @@ import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import org.jetbrains.annotations.NotNull;
+
 public class DAGraphDecomposer<V> {
 
     private final DAGraph<V> graph;
@@ -24,50 +26,37 @@ public class DAGraphDecomposer<V> {
     }
 
     public Map<Set<V>, DAGraph<V>> decomposeFrom(List<V> roots) {
-        // Paint every vertex with the 'color' of its root.
-        var colors = new HashMap<V, V>();
-        for (V root : roots) {
-            paintFrom(root, root, colors);
-        }
-        System.out.println("| decomposeFrom: " + roots.size() + " roots: colors=" + colors);
+        var colors = paintWithRootColors(roots);
 
         // Traverse the graph, detecting mis-colorings:
         // If the color of a vertex differs form the color of its root, then it has been re-painted from another root.
         // It means, that sub-graphs, starting at these roots, are connected.
         // Will track this connectedness in the 'parent' hash map.
-        var parents = new HashMap<V, V>();
-        for (V root : roots) {
-            parents.put(root, root);
-        }
-        for (V root : roots) {
-            checkColorFrom(root, root, colors, parents);
-        }
+        var parents = computeParents(roots, colors);
 
-        System.out.println("| decomposeFrom: parents");
-        parents.forEach((key, value) -> System.out.println("  key=" + key + "\tvalue=" + value));
-        System.out.println("| decomposeFrom...");
-
-        // "Union set find"
-        var realParents = new HashMap<V, V>();
-        roots.forEach(v -> {
-            var curN = v;
-            while (true) {
-                var parent = parents.get(curN);
-                if (parent == curN) break;
-                curN = parent;
-            }
-            realParents.put(v, curN);
-        });
+        var realParents = unionSetFind(roots, parents);
 
         System.out.println("| decomposeFrom: realParents");
-        realParents.forEach((key, value) -> System.out.println("  key=" + key + "\tvalue=" + value));
-        System.out.println("| decomposeFrom...");
+        dumpMap(realParents);
 
-        // Invert 'parents' to produce 'rootGroups'
-        var rootGroups = realParents.entrySet().stream()
-                .collect(Collectors.groupingBy(Map.Entry::getValue, Collectors.mapping(Map.Entry::getKey, Collectors.toSet())));
+        var rootGroups = invertToSets(realParents);
 
-        // Create sub-graphs for each root group
+        return subGraphsForRoots(rootGroups);
+    }
+
+    private static <V> Map<V, Set<V>> invertToSets(HashMap<V, V> realParents) {
+        return realParents
+                .entrySet()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Map.Entry::getValue,
+                                Collectors.mapping(Map.Entry::getKey, Collectors.toSet())
+                        )
+                );
+    }
+
+    private HashMap<Set<V>, DAGraph<V>> subGraphsForRoots(Map<V, Set<V>> rootGroups) {
         System.out.println("| decomposeFrom " + rootGroups.size() + " rootGroups");
         var result = new HashMap<Set<V>, DAGraph<V>>();
         for (Set<V> rootGroup : rootGroups.values()) {
@@ -83,6 +72,50 @@ public class DAGraphDecomposer<V> {
         }
 
         return result;
+    }
+
+    private static <V> void dumpMap(HashMap<V, V> realParents) {
+        realParents.forEach((key, value) -> System.out.println("  key=" + key + "\tvalue=" + value));
+    }
+
+    private static <V> @NotNull HashMap<V, V> unionSetFind(List<V> roots, HashMap<V, V> parents) {
+        // "Union set find"
+        var realParents = new HashMap<V, V>();
+        roots.forEach(v -> {
+            var curN = v;
+            while (true) {
+                var parent = parents.get(curN);
+                if (parent == curN) break;
+                curN = parent;
+            }
+            realParents.put(v, curN);
+        });
+        return realParents;
+    }
+
+    private @NotNull HashMap<V, V> computeParents(List<V> roots, HashMap<V, V> colors) {
+        var parents = new HashMap<V, V>();
+        for (V root : roots) {
+            parents.put(root, root);
+        }
+        for (V root : roots) {
+            checkColorFrom(root, root, colors, parents);
+        }
+
+        System.out.println("| decomposeFrom: parents");
+        dumpMap(parents);
+        System.out.println("| decomposeFrom...");
+        return parents;
+    }
+
+    private @NotNull HashMap<V, V> paintWithRootColors(List<V> roots) {
+        // Paint every vertex with the 'color' of its root.
+        var colors = new HashMap<V, V>();
+        for (V root : roots) {
+            paintFrom(root, root, colors);
+        }
+        System.out.println("| decomposeFrom: " + roots.size() + " roots: colors=" + colors);
+        return colors;
     }
 
     private void paintFrom(V vertex, V color, HashMap<V, V> colors) {
