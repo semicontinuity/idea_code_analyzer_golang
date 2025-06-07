84493eb5644e05baee4b3a5f3944acef5d2ec5c7
semicontinuity <igor.a.karpov@gmail.com>
2025-06-07 13:27:38 +0300

    DAGraphSegregator

--- src/main/java/semicontinuity/idea/code/analyzer/graph/DAGraphViewRenderer.kt
+++ src/main/java/semicontinuity/idea/code/analyzer/graph/DAGraphViewRenderer.kt
@@ -6,29 +6,17 @@ import java.util.stream.Collectors
 
 class DAGraphViewRenderer<V, VERTEX_PAYLOAD, COMP, IND_COMPS : COMP, VERTEX : COMP, SPLIT : COMP, LAYER : COMP, SORT_KEY : Comparable<SORT_KEY>?>(
     private val graph: DAGraph<V>,
-    viewFactory: Factory<VERTEX_PAYLOAD, COMP, IND_COMPS, VERTEX, SPLIT, LAYER>,
-    payloadFunction: Function<V, VERTEX_PAYLOAD>,
-    sortKeyFunction: Function<V, SORT_KEY>
+    private val viewFactory: Factory<VERTEX_PAYLOAD, COMP, IND_COMPS, VERTEX, SPLIT, LAYER>,
+    private val payloadFunction: Function<V, VERTEX_PAYLOAD>,
+    private val sortKeyFunction: Function<V, SORT_KEY>,
 ) {
-    private val viewFactory: Factory<VERTEX_PAYLOAD, COMP, IND_COMPS, VERTEX, SPLIT, LAYER>
-    private val payloadFunction: Function<V, VERTEX_PAYLOAD>
-    private val sortKeyFunction: Function<V, SORT_KEY>
-    private val multiPredecessorVertices: Set<V>
+    private val multiPredecessorVertices: Set<V> =
+        graph.vertices().stream()
+            .filter { v: V -> graph.predecessorCount(v) > 1 }
+            .collect(Collectors.toSet())
 
-    init {
-        this.viewFactory = viewFactory
-        this.payloadFunction = payloadFunction
-        this.multiPredecessorVertices =
-            graph.vertices().stream().filter { v: V -> graph.predecessorCount(v) > 1 }.collect(
-                Collectors.toSet()
-            )
-        this.sortKeyFunction = sortKeyFunction
-    }
-
-    fun render(): COMP {
-        val view = doRender(graph) ?: return viewFactory.newIndependentComponents(listOf<COMP>())
-        return view
-    }
+    fun render(): COMP =
+        doRender(graph) ?: viewFactory.newIndependentComponents(listOf<COMP>())
 
     private fun doRender(graph: DAGraph<V>): COMP? {
         if (!graph.hasVertices()) return null
@@ -63,16 +51,15 @@ class DAGraphViewRenderer<V, VERTEX_PAYLOAD, COMP, IND_COMPS : COMP, VERTEX : CO
             //     }
             //     .collect(Collectors.toList())
             val components = decomposed.entries
-                .map { this.renderRootsWithSubgraph(it) }
+                .map { (roots, subGraph) ->
+                    this.renderRootsWithSubgraph(roots, subGraph)
+                }
 
             independentCompsIfManyOrNullIfEmpty(components)
         }
     }
 
-    private fun renderRootsWithSubgraph(rootsWithSubgraph: Map.Entry<Set<V>, DAGraph<V>>): COMP? {
-        val roots = rootsWithSubgraph.key
-        val subGraph = rootsWithSubgraph.value
-
+    private fun renderRootsWithSubgraph(roots: Set<V>, subGraph: DAGraph<V>): COMP? {
         val rootsViews = roots.stream()
             .sorted(Comparator.comparing<V, SORT_KEY>(sortKeyFunction))
             .map { r: V ->
@@ -87,6 +74,7 @@ class DAGraphViewRenderer<V, VERTEX_PAYLOAD, COMP, IND_COMPS : COMP, VERTEX : CO
             else -> viewFactory.newSplit(rootsViews, subGraphView)
         }
     }
+
     private fun independentCompsIfManyOrNullIfEmpty(items: List<COMP?>): COMP? =
         if (items.isEmpty()) {
             null
@@ -101,15 +89,13 @@ class DAGraphViewRenderer<V, VERTEX_PAYLOAD, COMP, IND_COMPS : COMP, VERTEX : CO
             viewFactory.newIndependentComponents(items)
         }
 
-    private fun vertexViews(graph: DAGraph<V>, isMultiVertex: Boolean): List<VERTEX> {
-        return graph.vertices()
+    private fun vertexViews(graph: DAGraph<V>, isMultiVertex: Boolean): List<VERTEX> =
+        graph.vertices()
             .stream()
             .filter { v: V -> multiPredecessorVertices.contains(v) == isMultiVertex }
             .sorted(Comparator.comparing(sortKeyFunction))
             .map { vertex: V ->
-                val payload = payloadFunction.apply(vertex)
-                viewFactory.newVertex(payload)
+                viewFactory.newVertex(payloadFunction.apply(vertex))
             }
             .collect(Collectors.toList())
-    }
 }
